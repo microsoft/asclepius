77d5bf6fac2553f4ab3faf4267f34fd567f98077
Phil Jirsa <phil.jirsa@microsoft.com>
2024-04-03 00:35:17 +0000

    refactor to use weather from api

--- src/Mercy.LabSummarization.Api/function_app.py
+++ src/Mercy.LabSummarization.Api/function_app.py
@@ -27,3 +27,14 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+        
+@app.route(route="weather", auth_level=func.AuthLevel.ANONYMOUS)
+def weather(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # load weather data from utf-8-sig encoded json file
+    with open('data/weather.json', 'r', encoding='utf-8-sig') as file:
+        weather = json.load(file)
+        
+    # return weather as json
+    return func.HttpResponse(json.dumps(weather), mimetype="application/json")
